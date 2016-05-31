41b5d70a0af5adf8a17e60989d9b5148b6b61d4a
James Morrow <james@jamesmorrowdesign.com>
2016-05-30 23:39:10 -0600

    Blend.draw().

--- src/paths/Blend.java
+++ src/paths/Blend.java
@@ -18,13 +18,20 @@ public class Blend {
 	 * @param b The second path
 	 * @param numVertices Determines how precisely the drawn path is approximated.
 	 * @param blendAmt The interpolation amount, a value from [0,1].
+	 * @param pa The Processing instance to draw to.
 	 */
-	public static void draw(Path a, Path b, int numVertices, float blendAmt) {
+	public static void draw(Path a, Path b, int numVertices, float blendAmt, PApplet pa) {
 		float amt = 0;
 		float dAmt = 1f / numVertices;
+		
+		pa.beginShape();
 		for (int i=0; i<numVertices; i++) {
 			a.trace(ptA, amt);
+			b.trace(ptB, amt);
+			pa.vertex(PApplet.lerp(ptA.x, ptB.x, blendAmt), 
+	                  PApplet.lerp(ptA.y, ptB.y, blendAmt));
 		}
+		pa.endShape();
 	}
 	
 	/**
