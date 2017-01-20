d13ca14c64cf16308e05ad5dde6036ed3cc72297
James Morrow <jamesmorrowdesign@gmail.com>
2017-01-19 23:21:29 -0500

    Added very basic and not-yet-optimized Voronoi implementation.

---
+++ src/render/RenderVoronoi.java
@@ -0,0 +1,55 @@
+package render;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import processing.core.PApplet;
+import processing.core.PGraphics;
+import tracer.Point;
+
+/**
+ * 
+ * @author James Morrow [jamesmorrowdesign.com]
+ *
+ */
+public class RenderVoronoi extends Render {
+    protected int c1 = 0xff000000;
+    protected int c2 = 0xffffffff;
+    
+    public RenderVoronoi(List<Point> pts) {
+        super(pts);
+    }
+    
+    public RenderVoronoi(Point[] pts) {
+        super(pts);
+    }
+
+    @Override
+    public void draw(PGraphics g) {
+        if (pts.size() > 0) {
+            g.loadPixels();
+            
+            int i=0;
+            for (float x=0; x<g.width; x++) {
+                for (float y=0; y<g.height; y++) {
+                    int closestIndex = 0;
+                    float minDistSq = (pts.get(0).y-y)*(pts.get(0).y-y) + (pts.get(0).x-x)*(pts.get(0).x-x);
+                    for (int j=1; j<pts.size(); j++) {
+                        Point pt = pts.get(j);
+                        float distSq = (pt.y-y)*(pt.y-y) + (pt.x-x)*(pt.x-x);
+                        if (distSq < minDistSq) {
+                            minDistSq = distSq;
+                            closestIndex = j;
+                        }
+                    }
+                    int color = PApplet.lerpColor(c1, c2, PApplet.map(closestIndex, 0, pts.size(), 0, 1), RGB);
+                    g.pixels[i] = color;
+                    i++;
+                }
+            }
+
+            g.updatePixels();
+        }
+    }
+
+}
